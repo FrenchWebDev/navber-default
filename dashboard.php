0fb2a2b91e7223ce3ffbf6d1b89f592f5c505c6e
/Разработчик <98223371+FrenchWebDev@users.noreply.github.com>
2022-07-04 11:04:22 +0200

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dashboard.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <a id="reload" href="#">Dashboard</a>
+
+            <div class="elements">
+                <a href="" class="element">content1</a>
+                <a href="" class="element">content2</a>
+                <a href="" class="element">content3</a>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
